325ac929c680f7f0504d5040fc5cb1c3c05d0340
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-02-14 13:09:31 -0700

    chore: green_enemy hitbox, hurtbox logic and connection with stats_component, tutorial 8

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,6 +5,15 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
+
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+	)
+	stats_component.no_health.connect(queue_free)
